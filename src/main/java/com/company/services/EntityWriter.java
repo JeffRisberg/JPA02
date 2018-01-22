81f5a7cc8a3272cd7a268f7c0e6a840698f5655a
JeffRisberg <jrisberg@aol.com>
2018-01-21 21:24:39 -0800

    took libraries out of git repo

--- src/main/java/com/company/services/EntityWriter.java
+++ src/main/java/com/company/services/EntityWriter.java
@@ -1,8 +1,17 @@
 package com.company.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author Jeff Risberg
  * @since 01/13/18
  */
+@Slf4j
 public class EntityWriter {
+    protected ObjectMapper objectMapper;
+
+    public EntityWriter() {
+        this.objectMapper = new ObjectMapper();
+    }
 }
