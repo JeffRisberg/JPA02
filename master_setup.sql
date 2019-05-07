0c115bac241b8da5436f7e519779aae065916aed
JeffRisberg <jrisberg@aol.com>
2019-05-07 09:51:00 -0700

    added sample population

--- master_setup.sql
+++ master_setup.sql
@@ -8,3 +8,20 @@ CREATE TABLE tenants (
 
 INSERT INTO tenants(name) values ('T1');
 INSERT INTO tenants(name) values ('T2');
+
+DELETE FROM orders;
+DELETE FROM donations;
+DELETE FROM donors;
+DELETE FROM charities;
+
+INSERT INTO charities (id, date_created, last_updated, name, ein, description)
+  values(1, now(), now(), "Red Cross", "55-77777777", "disaster relief");
+
+INSERT INTO charities (id, date_created, last_updated, name, ein, description)
+values(2, now(), now(), "American Cancer Soc", "44-4545454545", "medical research");
+
+INSERT INTO donors (id, date_created, last_updated, name, age)
+    values(1, now(), now(), "Bob Jones", 30);
+
+INSERT INTO donors (id, date_created, last_updated, name, age)
+values(2, now(), now(), "Sally Smith", 29);
